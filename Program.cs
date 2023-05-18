fde8a6b1f2031bf85610f7df1005ecfa04d96671
IWillSettYou <114726553+IWillSettYou@users.noreply.github.com>
2023-05-18 08:19:29 +0200

    Added uwu file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreateDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@".\");
+            List<DateTime> CreatedDates = new List<DateTime>();
+            string dik = "";
+            foreach (string s in files)
+            {
+                dik += s + File.GetCreationTime($@".\{s}");
+            }
+            File.WriteAllText(@".\text.txt", dik);
+        }
+    }
+}
